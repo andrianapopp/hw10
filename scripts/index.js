803b0c6596a2d550e6d313a6c58292e54313aa80
Andriana Popp <115152248+andrianapopp@users.noreply.github.com>
2023-03-23 16:22:38 +0200

    stop?

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,29 @@
+
+// const year = prompt(`Напиши свой год рождения`)
+// const city = prompt(`Город, в котором ты живешь`)
+// const sport = prompt(`Твой любимый вид спорта`)
+//
+//
+// function cancel() {
+//     // const year = prompt(`Напиши свой год рождения`)
+//     if(!year) {
+//         alert(`Жаль, что Вы не захотели ввести свой год рождения`)
+//         return false
+//     }
+//     // const city = prompt(`Город, в котором ты живешь`)
+//     if(!city) {
+//         alert(`Жаль, что Вы не захотели ввести свой город`)
+//         return false
+//     }
+//     // const sport = prompt(`Твой любимый вид спорта`)
+//     if(!sport) {
+//         alert(`Жаль, что Вы не захотели ввести свой любимый вид спорта`)
+//         return false
+//     }
+// }
+// cancel()
+
+
 const year = prompt(`Напиши свой год рождения`)
 if(!year) {
     alert(`Жаль, что Вы не захотели ввести свой год рождения`)
@@ -41,11 +67,14 @@ function sportsman (sport) {
     }
 }
 
-
 if (year && city && sport) {
-
- if (city === `Киев` || city === `Берлин` || sport === `Париж` || sport === `Футбол`|| sport === `Баскетбол`|| city === `Бокс`) {
+    if (city === `Киев` || city === `Берлин` || city === `Париж` || sport === `Футбол` || sport === `Баскетбол` || sport === `Бокс`) {
         alert(`${age} лет. Ты живешь в ${city}, ${country(city)}. Ты хочешь стать как ${sportsman(sport)}`)
     } else {
-     alert(`${age} лет. Ты живешь в ${city}. Ты хочешь стать известным спортсменом!`)
- }}
+        alert(`${age} лет. Ты живешь в ${city}. Ты хочешь стать известным спортсменом!`)
+    }
+
+}
+
+
+
